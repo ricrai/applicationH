63341dfb5c36f181d5a30c3ed43da7e8f0b7c4bf
Mr <LonganPons@gnaiil.com>
2020-01-03 21:06:38 -0600

    Una comision que no aporta nada, pero aumenta el historial

--- src/appHversion1_2/Exe1_2.java
+++ src/appHversion1_2/Exe1_2.java
@@ -3,14 +3,15 @@ package appHversion1_2;
 public class Exe1_2 {
 
 	public static void main( String[] a ) {
+		System.out.println("Mensaje de prueba para hacer una modifikcion en el repositorio");
+		
+		
 		Ventaneando f = new Ventaneando();
-		f.setTituloGral( "BIENVENIDOS A LA VERSION 1.2 DE LA APP " );
+		f.setTituloGral( "BIENVENIDOS A LA VERSION 1.2 DE LA APPestosa " );
 		f.setResizable( false );
-	
-	
-	
-	
-	
+		
+		
+		
 	}//fin del metodo main
 
 } //fin de clase de EJECUCION
