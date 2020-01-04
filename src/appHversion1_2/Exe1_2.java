8b6453dac241325f20aea6e5ec993b9263d9e77c
Mr <LonganPons@gnaiil.com>
2020-01-04 11:20:16 -0600

    Comision para verificar la sincronizacion con GITHUB, sin cambios sustanciales

--- src/appHversion1_2/Exe1_2.java
+++ src/appHversion1_2/Exe1_2.java
@@ -3,8 +3,9 @@ package appHversion1_2;
 public class Exe1_2 {
 
 	public static void main( String[] a ) {
-		System.out.println("Mensaje de prueba para hacer una modifikcion en el repositorioRAMA MASTER");
 		
+		System.out.println("Mensaje de prueba para hacer una modifikcion en el repositorioRAMA MASTER");
+		System.out.println("Modificacion para actualizar los repositorio de GITHUB hecha con folio 040120-11818");
 		
 		Ventaneando f = new Ventaneando();
 		f.setTituloGral( "BIENVENIDOS A LA VERSION 1.2 DE LA APPestosa " );
